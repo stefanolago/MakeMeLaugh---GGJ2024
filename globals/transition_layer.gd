6c9c8ae2ef065d559e5687aab687bef51a396d98
stefanolago <stefanolago86@gmail.com>
2024-01-25 10:24:51 +0100

    Initial commit

---
+++ globals/transition_layer.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+@onready var animation_player: AnimationPlayer = $TransitionAnimationPlayer
+
+func change_scene(target: PackedScene) -> void:
+	animation_player.play("fade_to_black")
+	get_tree().paused = true
+	await animation_player.animation_finished
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(target)
+	animation_player.play_backwards("fade_to_black")
