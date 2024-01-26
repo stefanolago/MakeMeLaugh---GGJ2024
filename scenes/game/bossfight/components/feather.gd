5948a1eda60b370eecfcb3e358f43e22d16ae6d6
Libero91 <liberomasini91@gmail.com>
2024-01-26 18:58:15 +0100

    sound effect piuma

--- scenes/game/bossfight/components/feather.gd
+++ scenes/game/bossfight/components/feather.gd
@@ -25,7 +25,18 @@ var shake_meter: int = 0:
 			reset_to_normal()
 
 
-var feather_state: FeatherState = FeatherState.NORMAL
+var feather_state: FeatherState = FeatherState.NORMAL:
+	set(value):
+		match value:
+			FeatherState.NORMAL:
+				($feather as AudioStreamPlayer).playing = false
+				($feather as AudioStreamPlayer).stream_paused = true
+			FeatherState.IN_HAND:
+				if feather_state == FeatherState.NORMAL:
+					($feather as AudioStreamPlayer).playing = true
+					($feather as AudioStreamPlayer).stream_paused = true
+		feather_state = value
+
 var minimum_frames_to_register_shake: float = 200.0
 @export var total_shake_meter_to_reach: int = 200
 
@@ -35,7 +46,9 @@ func _input(event: InputEvent) -> void:
 		feather_area.global_position = get_global_mouse_position()
 		if feather_state == FeatherState.IN_HAND_AND_TICKLING and (event as InputEventMouseMotion).relative.length_squared() > minimum_frames_to_register_shake:
 			shake_meter += 1
-			
+			($feather as AudioStreamPlayer).stream_paused = false
+		else:
+			($feather as AudioStreamPlayer).stream_paused = true
 		#if (event as InputEventMouseMotion).relative.length_squared()
 	if event is InputEventMouseButton and event.is_released():
 		feather_state = FeatherState.NORMAL
@@ -52,6 +65,7 @@ func reset_to_normal() -> void:
 	feather_state = FeatherState.NORMAL
 	feather_area.global_position = global_position
 	shake_meter = 0
+	
 
 
 func _on_feather_active_area_entered(_area:Area2D) -> void:
