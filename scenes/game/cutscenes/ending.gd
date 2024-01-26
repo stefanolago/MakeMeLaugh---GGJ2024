54af172f2a683f8b837b913c3987529dcc533afd
Libero91 <liberomasini91@gmail.com>
2024-01-26 22:23:25 +0100

    working on ending curscene

--- scenes/game/cutscenes/ending.gd
+++ scenes/game/cutscenes/ending.gd
@@ -1,13 +1,21 @@
 extends Control
 
 var credits:PackedScene = preload("res://scenes/_credits/credits.tscn") 
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Dialogic.signal_event.connect(DialogicSignal)
-	Dialogic.start("ending")
+	anim_player.play("ending_cinematic")
+	#Dialogic.start("ending")
 
 
 func DialogicSignal(argument:String) -> void:
 	if argument == "credits":
 		TransitionLayer.change_scene(credits)
+	if argument == "progress_animation":
+		Dialogic.paused = true
+		anim_player.active = true
+		print("continue playing")
+
+
