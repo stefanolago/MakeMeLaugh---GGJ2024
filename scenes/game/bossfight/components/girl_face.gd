48fed680b9bb5578413318e0431c4cfd690f966b
stefanolago <jafarrolo@gmail.com>
2024-01-27 00:37:13 +0100

    Fixed collision for girl

--- scenes/game/bossfight/components/girl_face.gd
+++ scenes/game/bossfight/components/girl_face.gd
@@ -2,10 +2,29 @@ extends Node2D
 
 class_name GirlFace
 
+enum State {
+	NORMAL,
+	INVINCIBLE
+}
+
+var state: State = State.NORMAL
+@onready var invincible_timer: Timer = $InvincibilityTimer
+@onready var active_material: ShaderMaterial = ($Sprite2D as Sprite2D).material
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	global_position = get_global_mouse_position()
 
 
-func _on_area_2d_body_entered(_body:Node2D) -> void:
-	print("HIT")
+func _on_area_2d_body_entered(body:Node2D) -> void:
+	body.queue_free()
+	if state != State.INVINCIBLE:
+		GameStats.girl_health -= 1
+		state = State.INVINCIBLE
+		invincible_timer.start()
+		active_material.set_shader_parameter("Strength", 20)
+
+
+func _on_invincibility_timer_timeout() -> void:
+	state = State.NORMAL
+	active_material.set_shader_parameter("Strength", 2)
