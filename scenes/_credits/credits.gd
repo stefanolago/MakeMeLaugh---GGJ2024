e431acd01217988f7d57c8dea01656d7f5945a4f
stefanolago <jafarrolo@gmail.com>
2024-01-27 02:45:07 +0100

    Fixes to cyclical dependency

--- scenes/_credits/credits.gd
+++ scenes/_credits/credits.gd
@@ -3,11 +3,11 @@ extends Control
 signal credits_back_button()
 
 @export var final_credits:bool = false
-var main_manu:PackedScene = preload("res://scenes/_start/start_menu.tscn") 
+var main_menu: String = "res://scenes/_start/start_menu.tscn"
 
 func _on_back_button_pressed() -> void:
 	visible = false
 	if final_credits:
-		TransitionLayer.change_scene(main_manu)
+		TransitionLayer.change_scene_to_file(main_menu)
 	else:
 		credits_back_button.emit()
